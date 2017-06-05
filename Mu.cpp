c6891f3cd401bb0432b46b6b1e38b55798ae9ca2
Miguel Raggi <mraggi@gmail.com>
2017-06-05 16:52:16 -0500

    GPU detection. Now automatically detects whether it should or should not use the GPU
    Former-commit-id: 04832d760bb63357aa53d47ca1f96a799330820d

--- Mu.cpp
+++ Mu.cpp
@@ -4,34 +4,59 @@
 
 Mu operator+(const Mu& A, const Mu& B)
 {
+#ifdef USE_GPU
 	Mu C(A.numrows());
 	C.m_M = A.m_M + B.m_M;
+#else
+	Mu C(A.numrows());
+	for (size_t x = 0; x < A.numrows(); ++x)
+	{
+		for (size_t y = 0; y < A.numcols(); ++y)
+		{
+			C(x,y) = A(x,y)+B(x,y);
+		}
+	}
+#endif
 	return C;
+
 }
+
 
 Mu operator*(const Mu& A, const Mu& B)
 {
 	Mu C(A.numrows());
+#ifdef USE_GPU
 	C.m_M = A.m_M * B.m_M;
+	return C;
+#else
+	for (size_t x = 0; x < A.numrows(); ++x)
+	{
+		for (size_t y = 0; y < A.numcols(); ++y)
+		{
+			C(x,y) = A(x,y)*B(x,y);
+		}
+	}
+#endif
 	return C;
 }
 
 void Mu::Realize(const vector<Point>& P, double Cmx, double Cmy)
 {
-// 	for (size_t x = 0; x < numcols(); ++x)
-// 	{
-// 		for (size_t y = 0; y < numrows(); ++y)
-// 		{
-// 			m_M[x][y] = -1;
-// 		}
-// 	}
-	
+#ifdef USE_GPU
 	m_M = af::constant(-1.0,m_n,m_n,f64);
-	
+#else
+	for (size_t x = 0; x < numcols(); ++x)
+	{
+		for (size_t y = 0; y < numrows(); ++y)
+		{
+			m_M[x][y] = -1.0;
+		}
+	}
+#endif
 	double dx = MaximaLongitudQueNoEs0(Cmx);
 	double dy = MaximaLongitudQueNoEs0(Cmy);
-	long dxi = long(dx+1);
-	long dyi = long(dy+1);
+	long dxi = long(dx+2);
+	long dyi = long(dy+2);
 	
 // 	cout << "di = " << di << endl;
 	
@@ -45,77 +70,76 @@ void Mu::Realize(const vector<Point>& P, double Cmx, double Cmy)
 		size_t minY = max(long(0),long(p.y)-dyi);
 		size_t maxY = min(long(numrows()), long(p.y)+dyi);
 		
+#ifdef USE_GPU
 		size_t szx = maxX-minX;
 		size_t szy = maxY-minY;
-		
 		af::array GK = af::gaussianKernel(szx,szy,1.0/sqrt(2.0*Cmx),1.0/sqrt(2.0*Cmy));
 		GK /= max<double>(GK);
 		
 		m_M(af::seq(minX,maxX-1),af::seq(minY,maxY-1)) *= (1.0-GK);
-		
-		
-		
-// 		for (size_t x = minX; x < maxX; ++x)
-// 		{
-// 			for (size_t y = minY; y < maxY; ++y)
-// 			{
-// 				double XX = (p.x-x)*(p.x-x);
-// 				double YY = (p.y-y)*(p.y-y);
-// 				m_M[x][y] *= (1.0-exp(-Cmx*XX - Cmy*YY));
-// 			}
-// 		}
-		
+#else
+		for (size_t x = minX; x < maxX; ++x)
+		{
+			for (size_t y = minY; y < maxY; ++y)
+			{
+				double XX = (p.x-x)*(p.x-x);
+				double YY = (p.y-y)*(p.y-y);
+				m_M[x][y] *= (1.0-exp(-Cmx*XX - Cmy*YY));
+			}
+		}
+#endif
 	}
 	
+#ifdef USE_GPU
 	m_M += 1.0;
-	
-// 	for (size_t x = 0; x < numcols(); ++x)
-// 	{
-// 		for (size_t y = 0; y < numrows(); ++y)
-// 		{
-// 			m_M[x][y] += 1;
-// 			if (abs(m_M[x][y]) < tolerance)
-// 				m_M[x][y] = 0.0;
-// 		}
-// 	}
-	
+#else
+	for (size_t x = 0; x < numcols(); ++x)
+	{
+		for (size_t y = 0; y < numrows(); ++y)
+		{
+			m_M[x][y] += 1.0;
+			if (abs(m_M[x][y]) < tolerance)
+				m_M[x][y] = 0.0;
+		}
+	}
+#endif
 }
 
 double Mu::Integrate() const
 {
-// 	double *host_a = af::sum(m_M).host<double>();        // must call array::free() later
-// 	double r = host_a[0];
-// 	af::freeHost(host_a);
-	
+#ifdef USE_GPU
 	return af::sum<double>(m_M);
-	
-// 	return r;
-// 	double result = 0.0;
-// 	for (size_t x = 0; x < numcols(); ++x)
-// 	{
-// 		for (size_t y = 0; y < numrows(); ++y)
-// 		{
-// 			result += m_M[x][y];
-// 		}
-// 	}
+#else
+	double result = 0.0;
+	for (size_t x = 0; x < numcols(); ++x)
+	{
+		for (size_t y = 0; y < numrows(); ++y)
+		{
+			result += m_M[x][y];
+		}
+	}
 // 	double n = m_M.size();
 // 	return result/(n*n);
-// 	return result;
+	return result;
+#endif
 }
 
 std::ostream& operator<<(std::ostream& os, const Mu& C)
 {
+#ifdef USE_GPU
 	af_print(C.get_array());
-// 	const size_t maxtoprint = 30;
-// 	os << '\n';
-// 	os << setprecision(3);
-// 	for (size_t x = 0; x < min(C.numcols(),maxtoprint); ++x)
-// 	{
-// 		for (size_t y = 0; y < min(C.numrows(),maxtoprint); ++y)
-// 		{
-// 			os << C(x,y) << '\t';
-// 		}
-// 		os << '\n';
-// 	}
+#else
+	const size_t maxtoprint = 30;
+	os << '\n';
+	os << setprecision(3);
+	for (size_t x = 0; x < min(C.numcols(),maxtoprint); ++x)
+	{
+		for (size_t y = 0; y < min(C.numrows(),maxtoprint); ++y)
+		{
+			os << C(x,y) << '\t';
+		}
+		os << '\n';
+	}
+#endif
 	return os;
 }
--- Mu.hpp
+++ Mu.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include <vector>
-#include <arrayfire.h>
+
+#ifdef USE_GPU
+	#include <arrayfire.h>
+#endif
+
 #include "Point.hpp"
 #include "TimeHelpers.hpp"
 #include "ReadFile.hpp"
@@ -21,18 +25,26 @@ inline double MaximaLongitudQueNoEs0(double C)
 class Mu
 {
 public:
+#ifdef USE_GPU
 	Mu(size_t n) : m_n(n), m_M(af::constant(0,n,n,f64))
 	{ 
 	}
+#else
+	Mu(size_t n) : m_n(n), m_M(n,Row(n,0))
+	{ 
+	}
+#endif
 	
-// 	double operator()(size_t x, size_t y) const
-// 	{
-// 		return m_M[x][y];
-// 	}
-// 	double& operator()(size_t x, size_t y)
-// 	{
-// 		return m_M[x][y];
-// 	}
+#ifndef USE_GPU
+	double operator()(size_t x, size_t y) const
+	{
+		return m_M[x][y];
+	}
+	double& operator()(size_t x, size_t y)
+	{
+		return m_M[x][y];
+	}
+#endif
 	size_t numcols() const { return m_n; }
 	size_t numrows() const { return m_n; }
 	
@@ -40,10 +52,12 @@ public:
 	
 	double Integrate() const;
 	
+#ifdef USE_GPU
 	const af::array& get_array() const
 	{
 		return m_M;
 	}
+#endif
 	
 	friend Mu operator+(const Mu& A, const Mu& B);
 	friend Mu operator*(const Mu& A, const Mu& B);
@@ -51,7 +65,11 @@ public:
 private:
 // 	Matrix m_M;
 	size_t m_n;
+#ifdef USE_GPU
 	af::array m_M;
+#else
+	Matrix m_M;
+#endif
 };
 
 std::ostream& operator<<(std::ostream& os, const Mu& C);
